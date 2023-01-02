4de54a875f767172dbad8ca21103d66795bb27bd
billzoygkos <67504030+billzoygkos@users.noreply.github.com>
2023-01-03 00:02:57 +0200

    Add files via upload

---
+++ vMenuServer/MainServer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using Newtonsoft.Json;
+using static vMenuServer.DebugLog;
+using static vMenuShared.ConfigManager;
+using vMenuShared;
+
+namespace vMenuServer
+{
+
+    public static class DebugLog
+    {
+        public enum LogLevel
+        {
+            error = 1,
+            success = 2,
+            info = 4,
+            warning = 3,
+            none = 0
+        }
+
+        /// <summary>
+        /// Global log data function, only logs when debugging is enabled.
+        /// </summary>
+        /// <param name="data"></param>
+        public static void Log(dynamic data, LogLevel level = LogLevel.none)
+        {
+            if (MainServer.DebugMode || level == LogLevel.error || level == LogLevel.warning)
+            {
+                string prefix = "[vMenu] ";
+                if (level == LogLevel.error)
+                {
+                    prefix = "^1[vMenu] [ERROR]^7 ";
+                }
+                else if (level == LogLevel.info)
+                {
+                    prefix = "^5[vMenu] [INFO]^7 ";
+                }
+                else if (level == LogLevel.success)
+                {
+                    prefix = "^2[vMenu] [SUCCESS]^7 ";
+                }
+                else if (level == LogLevel.warning)
+                {
+                    prefix = "^3[vMenu] [WARNING]^7 ";
+                }
+                Debug.WriteLine($"{prefix}[DEBUG LOG] {data.ToString()}");
+            }
+        }
+    }
+}
